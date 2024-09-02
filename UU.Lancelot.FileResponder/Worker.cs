f36d64b89cdfb500f1d6937b845b70c6051d7f38
Marcus0035 <marek.dvoracek606@gmail.com>
2024-09-02 12:29:11 +0200

    #1

--- UU.Lancelot.FileResponder/Worker.cs
+++ UU.Lancelot.FileResponder/Worker.cs
@@ -15,9 +15,29 @@ public class Worker : BackgroundService
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation(Jokes.GetJoke());
             }
-            await Task.Delay(1000, stoppingToken);
+            await Task.Delay(5000, stoppingToken);
         }
+    }
+}
+
+public class Jokes
+{
+    static string[] jokesArray = 
+    {
+    "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "I'm reading a book on the history of glue. I just can't seem to put it down.",
+    "Why don't scientists trust atoms? Because they make up everything.",
+    "I told my wife she should embrace her mistakes. She gave me a hug.",
+    "I'm reading a book on anti-gravity. It's impossible to put down.",
+    "I used to play piano by ear, but now I use my hands."
+    };
+
+    public static string GetJoke()
+    {
+        Random random = new Random();
+        int index = random.Next(jokesArray.Length);
+        return jokesArray[index];
     }
 }
